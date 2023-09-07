5acad82f293b94ba38c677d29ed9871dcde88bca
esstefaniagonzalez <estefaniagonzalezg9@gmail.com>
2023-09-06 21:00:23 -0300

    modificaciones

--- 1/main.js
+++ 1/main.js
@@ -84,3 +84,23 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   });
+
+
+
+  /*---------------------------*/
+
+  fetch('productos.json')
+    .then(response => response.json())
+    .then(data => {
+        const productos = data.productos;
+
+        const productosContainer = document.getElementById('productosContainer');
+        productos.forEach(producto => {
+            const accesoriosElement = document.createElement('p');
+            accesoriosElement.textContent = `Nombre: ${producto.nombre}, Precio: $${producto.precio}`;
+            productosContainer.appendChild(accesoriosElement);
+        });
+    })
+    .catch(Error => {
+        console.error('Ha ocurrido un error');
+    });
